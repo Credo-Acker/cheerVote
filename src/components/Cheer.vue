27a9345052b14aaa16f7b0f02c004336b8048e0a
CredoM <942794848@qq.com>
2018-04-19 09:00:49 +0800

    处理计算机

--- src/components/Cheer.vue
+++ src/components/Cheer.vue
@@ -159,7 +159,7 @@ export default {
                 }
             }
 
-            if (isNaN(this.cheer1) || isNaN(this.cheer2) || isNaN(this.cheer3) || isNaN(this.cheer4) || isNaN(this.cheer5) || isNaN(this.cheer6) || isNaN(this.cheer7) || isNaN(this.cheer8) || isNaN(this.cheer9) || isNaN(this.cheer10) || isNaN(this.cheer11) || isNaN(this.cheer12) || isNaN(this.cheer13) || this.cheer1 < 0 || this.cheer2 || this.cheer3 < 0 || this.cheer4 < 0 || this.cheer5 < 0 || this.cheer6 < 0 ||  this.cheer7 < 0 || this.cheer8 < 0 || this.cheer9 < 0 || this.cheer10 < 0 || this.cheer11 < 0 || this.cheer12 < 0 || this.cheer13 < 0) {
+            if (isNaN(this.cheer1) || isNaN(this.cheer2) || isNaN(this.cheer3) || isNaN(this.cheer4) || isNaN(this.cheer5) || isNaN(this.cheer6) || isNaN(this.cheer7) || isNaN(this.cheer8) || isNaN(this.cheer9) || isNaN(this.cheer10) || isNaN(this.cheer11) || isNaN(this.cheer12) || isNaN(this.cheer13) || this.cheer1 < 0 || this.cheer2 < 0 || this.cheer3 < 0 || this.cheer4 < 0 || this.cheer5 < 0 || this.cheer6 < 0 ||  this.cheer7 < 0 || this.cheer8 < 0 || this.cheer9 < 0 || this.cheer10 < 0 || this.cheer11 < 0 || this.cheer12 < 0 || this.cheer13 < 0) {
                 alert("输入不合法，请重新输入！");
                 this.cheer1 = "";
                 this.cheer2 = "";
@@ -185,7 +185,6 @@ export default {
         free_num: function () {
             if ((this.cheerNum - this.totle_num) < 0) {
                 alert("剩余可投票数不够，请重新分配！");
-                console.log(this.cheerNum);
                 this.cheer1 = "";
                 this.cheer2 = "";
                 this.cheer3 = "";
